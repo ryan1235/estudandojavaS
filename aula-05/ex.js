3ff9f918ab811239b90dfa18bb24aab4138d09b1
ryan1235 <58185393+ryan1235@users.noreply.github.com>
2022-01-12 13:10:15 -0300

    :bellhop_bell: novo exercícios!

--- aula-05/ex.js
+++ aula-05/ex.js
@@ -1,46 +1,40 @@
-var img;
-        window.onload = function () {
-            img = document.getElementById('imagem')
-        }
-var data = new Date()
-var realano = data.getFullYear()
-var ano = document.getElementById('txtnuber')
-var res = document.getElementById('res')
-var fsex = document.getElementsByClassName('radsex')
-var idade = realano - Number(ano.value)
-
 function clicar() {
-
-    if (ano.value.length == 0 || ano.value > realano) {
+    var data = new Date()
+    var realano = data.getFullYear()
+    var ano = document.getElementById('txtnuber')
+    var res = document.getElementById('res')
+    if (ano.value == 0 || ano.value > realano) {
         alert('[ERRO] Verifique os dados e tente novamente')
     } else {
+        var fsex = document.getElementsByClassName('radsex')
+        var idade = realano - Number(ano.value)
+        var genero = ''
+        var img = document.getElementById('imagem')
         if (fsex[0].checked) {
-            var genero = ''
-            genero = 'masculino'
+            var genero = 'masculino'
             if (idade >= 0 && idade < 10) {
-                img.src = '/img/CriancaHomem.jpg'
-            } else if (idade < 18) {
-                img.src = '/img/JovemHomem.jpg'
-            } else if (idade < 60) {
-                img.src = '/img/AdultoHomem.jpg'
+                img.src = '/estudandojavaS/img/CriancaHomem.jpg'
+            } else if (idade > 10 && idade < 18) {
+                img.src = '/estudandojavaS/img/JovemHomem.jpg'
+            } else if (idade > 18 && idade < 60) {
+                img.src = '/estudandojavaS/img/AdultoHomem.jpg'
             } else {
-                img.src = '/img/IdosoHomem.jpg'
-            }
-        } else {
+                img.src = '/estudandojavaS/img/IdosoHomem.jpg'
+            }}else {
 
 
-            genero = 'feminino'
+            var genero = 'feminino'
             if (idade >= 0 && idade < 10) {
-                img.src = '/img/CriancaMulher.jpg'
-            } else if (idade < 18) {
-                img.src = '/img/JovemMulher.jpg'
-            } else if (idade < 60) {
-                img.src = '/img/AdultoMulher.jpg'
+                img.src = '/estudandojavaS/img/CriancaMulher.jpg'
+            } else if (idade > 10 && idade < 18) {
+                img.src = '/estudandojavaS/img/JovemMulher.jpg'
+            } else if (idade > 18 && idade < 60) {
+                img.src = '/estudandojavaS/img/AdultoMulher.jpg'
             } else {
-                img.src = '/img/IdosoMulher.jpg'
+                img.src = '/estudandojavaS/img/IdosoMulher.jpg'
             }
         }
 
-        res.innerText == `você tem ${idade} anos`
+        res.innerText = `você tem ${idade} anos`
     }
 }
