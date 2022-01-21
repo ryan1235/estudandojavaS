1c5cbf872187cc32bccb8bdc91f0eb1e14c654f4
ryan1235 <58185393+ryan1235@users.noreply.github.com>
2022-01-21 03:39:01 -0300

    :bellhop_bell: novo exercícios!

---
+++ aula-06/ex.js
@@ -0,0 +1,32 @@
+var n1 = document.getElementById('nu1')
+var n2 = document.getElementById('nu2')
+var n3 = document.getElementById('nu3')
+var ress = document.getElementById('res')
+function contar() {
+    if(n1.value == 0 || n2.value == 0) {
+        ress.innerHTML = '<p>Impossivel contar</p>'
+        alert('[Erro] Prencha todos os valores')
+    } else {
+        ress.innerHTML = '<p>Contando...</p>'
+        let Inicio = Number(n1.value)
+        let fim = Number(n2.value)
+        let passo = Number(n3.value)
+        if(passo <= 0){
+            alert('Passo invalido foi considera passo 1')
+            passo = 1
+        }
+        if(Inicio < fim) {
+            //contagem crecente
+            for (let c = Inicio; c <= fim; c += passo) {
+                ress.innerHTML += `${c}\u{27A1}`
+            }
+            }else{
+                //cpmtagem regresiva
+                for(let c = Inicio; c >= fim; c -= passo){
+                    ress.innerHTML += `${c}\u{27A1}`  
+                }
+            }
+            ress.innerHTML += ` \u{1F3C1}`
+        }
+
+    }
