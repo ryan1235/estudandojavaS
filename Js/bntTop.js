88f97d82935352b9957203735ea68390de87cb27
ryan1235 <58185393+ryan1235@users.noreply.github.com>
2022-01-21 18:12:01 -0300

    :heartbeat: Novas funcionalidades
    Botão de volta ao topo

---
+++ Js/bntTop.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    $(".btnTop").hide()
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 150){
+            $(".btnTop").fadeIn()
+        }else{
+            $(".btnTop").fadeOut()
+        }
+    })
+    $(".btnTop").click(function(){
+        $("html,body").animate({
+            scrollTop: 0
+        },1000)
+    })
+})
+
